5946e63ce1f166dd5fcb19c072220a3c254a0214
kissa1001 <kissa101295@gmail.com>
2017-10-24 12:22:00 -0700

    added a fix for sorting

--- server/main.js
+++ server/main.js
@@ -5,7 +5,8 @@ import { Comments } from '../imports/collections/comments';
 Meteor.startup(() => {
 
   Meteor.publish('posts', function(per_page) {
-    return Posts.find({}, {sort: {date: -1}}, { limit: per_page });
+find({},{sort: {priority: 1}, limit: 2})
+    return Posts.find({}, {sort: {date: -1}, limit: per_page } );
   })
   Meteor.publish('comments', function(per_page) {
     return Comments.find({}, { limit: per_page });
